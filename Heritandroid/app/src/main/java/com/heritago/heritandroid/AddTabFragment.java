3adf8daab5217022e8342b83f99f0ba016f1c2e0
onur <onurtokoglu@gmail.com>
2017-03-27 15:51:07 +0300

    created tab navigation for tabs search, add and profile

---
+++ Heritandroid/app/src/main/java/com/heritago/heritandroid/AddTabFragment.java
@@ -0,0 +1,32 @@
+package com.heritago.heritandroid;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by onurtokoglu on 27/03/2017.
+ */
+
+public class AddTabFragment extends Fragment{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.search_tab_fragment, container, false);
+        TextView textView = (TextView) view.findViewById(R.id.textView);
+        textView.setText("Add");
+        return view;
+    }
+
+
+
+}
---
+++ Heritandroid/app/src/main/java/com/heritago/heritandroid/ProfileTabFragment.java
@@ -0,0 +1,29 @@
+package com.heritago.heritandroid;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class ProfileTabFragment extends Fragment{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.search_tab_fragment, container, false);
+        TextView textView = (TextView) view.findViewById(R.id.textView);
+        textView.setText("Profile");
+        return view;
+    }
+
+
+
+}
