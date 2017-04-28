e8f63e636c716f3af568a60244674439638c2bf7
fkucuk <fkucuk@gmail.com>
2017-04-28 23:35:06 +0300

    comment added

--- heritago/heritages/tests.py
+++ heritago/heritages/tests.py
@@ -1,4 +1,10 @@
 from django.test import TestCase
+from heritages.models import Tag
 
-# Create your tests here.
-# this will be test
+class TagTestCase(TestCase):
+    def setUp(self):
+        Tag.objects.create(name="Osman")
+
+    def test_tag_initial(self):
+        osmanTag = Tag.objects.get(name="Osman")
+        self.assertEqual(osmanTag.name, "Osman")
