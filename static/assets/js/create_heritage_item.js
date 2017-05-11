4664b644d7d3145bc263d7ae85c0bd86bd905447
Kartal Erkoc <kartalerkoc@gmail.com>
2017-05-11 20:54:01 +0300

    Updates create-heritage-item-page

--- static/assets/js/create_heritage_item.js
+++ static/assets/js/create_heritage_item.js
@@ -92,8 +92,6 @@ $(function() {
 
 
 
-
-
     AnyTime.picker( "create_new_heritage_item_start_date",
         {
             format: "%e %b %Y %E",
@@ -154,6 +152,94 @@ $(function() {
         }
     });
 
+
+    $('body').on('click', '.add-free-origin-field-but', function() {
+        var free_origin_field_value = $(this).parent().prev().children().eq(0).children().eq(0).val();
+
+        if (free_origin_field_value == "") {
+
+        }
+        else {
+            $(this).parent().prev().children().eq(0).children().eq(0).prop('disabled', true);
+            $(this).parent().prev().children().eq(0).children().eq(1).text("");
+            $(this).removeClass( "add-free-origin-field-but" ).addClass("remove-free-origin-field-but").addClass("btn-danger");
+            $(this).children().eq(0).removeClass( "fa-plus" ).addClass("fa-minus");
+            $(this).parent().parent().after("<div class=\"col-md-12\"><div class=\"col-md-11\">"+
+                "<div class=\"form-group floating-label\">"+
+                "<input type=\"text\" class=\"form-control create_new_heritage_item_free_origin\">"+
+                "<label>Origin (free-text field)</label>"+
+                "</div>"+
+                "</div>"+
+                "<div class=\"col-md-1\">"+
+                "<button type=\"button\" class=\"btn ink-reaction btn-floating-action btn-primary add-free-origin-field-but\"><i class=\"fa fa-plus\"></i></button>"+
+                "</div></div>");
+        }
+
+    });
+
+    $('body').on('click', '.remove-free-origin-field-but', function() {
+        $(this).parent().parent().remove();
+    });
+
+    $('body').on('click', '.add-key-value-field-but', function() {
+        var key_field_input = $(this).parent().prev().prev().prev().children().eq(0).children().eq(0).val();
+        var value_field_input = $(this).parent().prev().children().eq(0).children().eq(0).val();
+
+        if (key_field_input == "" || value_field_input == "") {
+
+        }
+        else {
+            $(this).parent().prev().prev().prev().children().eq(0).children().eq(0).prop('disabled', true);
+            $(this).parent().prev().prev().prev().children().eq(0).children().eq(1).text("");
+            $(this).parent().prev().children().eq(0).children().eq(0).prop('disabled', true);
+            $(this).parent().prev().children().eq(0).children().eq(1).text("");
+            $(this).removeClass( "add-key-value-field-but" ).addClass("remove-key-value-field-but").addClass("btn-danger");
+            $(this).children().eq(0).removeClass( "fa-plus" ).addClass("fa-minus");
+            $(this).parent().parent().after("<div class=\"col-md-12\">"+
+                "<div class=\"col-md-5\">"+
+                "<div class=\"form-group floating-label\">"+
+                "<input type=\"text\" class=\"form-control\">"+
+                "<label>key</label>"+
+                "</div>"+
+                "</div>"+
+                "<div class=\"col-md-1\">"+
+                "&nbsp;"+
+                "</div>"+
+                "<div class=\"col-md-5\">"+
+                "<div class=\"form-group floating-label\">"+
+                "<input type=\"text\" class=\"form-control\">"+
+                "<label>value</label>"+
+                "</div>"+
+                "</div>"+
+                "<div class=\"col-md-1\">"+
+                "<button type=\"button\" class=\"btn ink-reaction btn-floating-action btn-primary add-key-value-field-but\"><i class=\"fa fa-plus\"></i></button>"+
+                "</div>"+
+                "</div>");
+        }
+
+
+    });
+
+    $('body').on('click', '.remove-key-value-field-but', function() {
+        $(this).parent().parent().remove();
+    });
+
+    var timeout_for_create_heritage_item_title_input;
+    $("#create_new_heritage_item_title").keypress(function(e) {
+        if (timeout_for_create_heritage_item_title_input) {clearTimeout(timeout_for_create_heritage_item_title_input);}
+        timeout_for_create_heritage_item_title_input = setTimeout(function () {
+            $("#create_new_heritage_item_errors").html("");
+        }, 150);
+    });
+
+    var timeout_for_create_heritage_item_description_input;
+    $("#create_new_heritage_item_description").keypress(function(e) {
+        if (timeout_for_create_heritage_item_description_input) {clearTimeout(timeout_for_create_heritage_item_description_input);}
+        timeout_for_create_heritage_item_description_input = setTimeout(function () {
+            $("#create_new_heritage_item_errors").html("");
+        }, 150);
+    });
+
     $( "#create_new_heritage_item_create_but" ).click(function() {
         console.log("Pin type map");
         console.dir(geo_location_pin_map);
@@ -163,57 +249,17 @@ $(function() {
         console.dir(geo_location_polygon_map);
         console.log(geo_location_polygon_map.markers[0].position.lat());
         console.log(geo_location_polygon_map.markers[0].position.lng());
+
+        var title_input = $( "#create_new_heritage_item_title" ).val();
+        var description_input = $( "#create_new_heritage_item_title" ).val();
+
+        if (title_input == "" || description_input == "") {
+            $( "#create_new_heritage_item_errors" ).html("<b>You should provide a title and a description while submitting a new heritage item.</b>");
+        }
+        else {
+            // submit form here
+        }
     });
-
-
-    $('body').on('click', '.add-free-origin-field-but', function() {
-        $(this).removeClass( "add-free-origin-field-but" ).addClass("remove-free-origin-field-but").addClass("btn-danger");
-        $(this).children().eq(0).removeClass( "fa-plus" ).addClass("fa-minus");
-        $(this).parent().parent().after("<div class=\"col-md-12\"><div class=\"col-md-11\">"+
-            "<div class=\"form-group floating-label\">"+
-            "<input type=\"text\" class=\"form-control create_new_heritage_item_free_origin\">"+
-            "<label>Origin (free-text field)</label>"+
-            "</div>"+
-            "</div>"+
-            "<div class=\"col-md-1\">"+
-            "<button type=\"button\" class=\"btn ink-reaction btn-floating-action btn-primary add-free-origin-field-but\"><i class=\"fa fa-plus\"></i></button>"+
-            "</div></div>");
-    });
-
-    $('body').on('click', '.remove-free-origin-field-but', function() {
-        $(this).parent().parent().remove();
-    });
-
-    $('body').on('click', '.add-key-value-field-but', function() {
-        $(this).removeClass( "add-key-value-field-but" ).addClass("remove-key-value-field-but").addClass("btn-danger");
-        $(this).children().eq(0).removeClass( "fa-plus" ).addClass("fa-minus");
-        $(this).parent().parent().after("<div class=\"col-md-12\">"+
-        "<div class=\"col-md-5\">"+
-            "<div class=\"form-group floating-label\">"+
-            "<input type=\"text\" class=\"form-control\">"+
-            "<label>key</label>"+
-            "</div>"+
-            "</div>"+
-            "<div class=\"col-md-1\">"+
-            "&nbsp;"+
-        "</div>"+
-        "<div class=\"col-md-5\">"+
-            "<div class=\"form-group floating-label\">"+
-            "<input type=\"text\" class=\"form-control\">"+
-            "<label>value</label>"+
-            "</div>"+
-            "</div>"+
-            "<div class=\"col-md-1\">"+
-            "<button type=\"button\" class=\"btn ink-reaction btn-floating-action btn-primary add-key-value-field-but\"><i class=\"fa fa-plus\"></i></button>"+
-            "</div>"+
-            "</div>");
-    });
-
-    $('body').on('click', '.remove-key-value-field-but', function() {
-        $(this).parent().parent().remove();
-    });
-
-
 
 
 
