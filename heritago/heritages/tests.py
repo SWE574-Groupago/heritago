e05db44835348aef59bf3aa0ee6cf831e6247054
fkucuk <fkucuk@gmail.com>
2017-05-13 17:16:46 +0300

    coverage is added, tests are merged into one file "tests.py"

--- heritago/heritages/tests.py
+++ heritago/heritages/tests.py
@@ -1,12 +1,21 @@
 from django.test import TestCase
 from heritages.models import Tag
 from heritages.models import Heritage
+from heritages.models import Multimedia
 
-class ModelsTestCase(TestCase):
+
+class ModelsTest(TestCase):
     def setUp(self):
         Tag.objects.create(name="Osman")
         title = "Test Mosque"
         Heritage.objects.create(title=title)
+        file = open("testfile.txt", "w")
+        file.close()
+        file = open("testfile2.txt", "w")
+        file.close()
+        Multimedia.objects.create(url = "B", file = "testfile.txt", heritage=Heritage.objects.get(title="Test Mosque"))
+        Heritage.objects.create(title="Selimiye Mosque")
+        Multimedia.objects.create(url = "A", file="testfile2.txt", heritage = Heritage.objects.get(title = "Selimiye Mosque"))
 
     def test_tag_initial(self):
         osmanTag = Tag.objects.get(name="Osman")
@@ -22,3 +31,21 @@ class ModelsTestCase(TestCase):
         Heritage.objects.get(title = "Test Mosque").delete()
         with self.assertRaises(Heritage.DoesNotExist):
             Heritage.objects.get(title="Test Mosque")
+
+    def test_multimedia_delete(self):
+        """Try to get deleted multimedia object.."""
+        Multimedia.objects.get(url = "A").delete()
+        with self.assertRaises(Multimedia.DoesNotExist):
+            Multimedia.objects.get(url = "A")
+
+
+import unittest
+from django.test import Client
+
+
+class APITests(unittest.TestCase):
+    def setUp(self):
+        """Creating a client"""
+        self.client = Client()
+
+
