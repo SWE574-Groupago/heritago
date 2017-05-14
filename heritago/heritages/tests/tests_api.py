0619e00213be302d3bfd63cdf0fa57d205496547
fkucuk <fkucuk@gmail.com>
2017-05-14 19:13:43 +0300

    more integration

--- heritago/heritages/tests/tests_api.py
+++ heritago/heritages/tests/tests_api.py
@@ -3,6 +3,11 @@ from django.test import Client
 
 heritagePath = '/api/v1/heritages/'
 
+postedHeritageItemId = 0
+
+def postHeritageItem():
+
+    return
 
 class APITests(unittest.TestCase):
     def setUp(self):
@@ -15,6 +20,7 @@ class APITests(unittest.TestCase):
 
     def test_heritages_addnewheritage_with_inadequateinfo(self):
         """POSTing with inadequate fields"""
+        """POST api/heritages"""
         response = self.client.post(heritagePath, {
             "title": "Ayasofya"
         })
@@ -23,6 +29,7 @@ class APITests(unittest.TestCase):
 
     def test_heritages_addnewheritage_withnoenddate(self):
         """POSTing with no endDate"""
+        """POST api/heritages"""
         response = self.client.post(heritagePath, {
             "title": "Helva",
             "description": "desc",
@@ -38,6 +45,7 @@ class APITests(unittest.TestCase):
 
     def test_heritages_addnewheritage(self):
         """POSTing with full info"""
+        """GET api/heritages"""
         r = self.client.post(heritagePath, {
             "title": "Santa Clause",
             "description": "Santa Claus, also known as Saint Nicholas, Saint Nick, Kris Kringle, Father Christmas, "
@@ -55,3 +63,26 @@ class APITests(unittest.TestCase):
         response = r.json()
         print(response["id"])
         self.assertIsInstance(response["id"], int)
+
+    def test_heritages_searchheritagesbykeyword_existing(self):
+        """GET api/heritages?keyword=kwrd"""
+        r = self.client.get(heritagePath + "?keyword=santa")
+        response = r.json()
+        self.assertGreaterEqual(len(response), 0)
+
+    def test_heritages_searchheritagesbykeyword_notexisting(self):
+        """GET api/heritages?keyword=kwrd"""
+        r = self.client.get(heritagePath + "?keyword=nonexistingitem")
+        response = r.json()
+        self.assertEqual(len(response), 0)
+
+    def test_heritages_getmultimediasofaheritageitem(self):
+        """GET api/heritages/1/multimedia"""
+        r = self.client.get(heritagePath + "1/multimedia")
+        response = r.json()
+        self.assertEqual(len(response), 0)
+
+    def test_heritages_trytogetnonexistingheritageitemsmultimedia(self):
+        """GET api/heritages/1/multimedia"""
+        r = self.client.get(heritagePath + "-1/multimedia")
+
