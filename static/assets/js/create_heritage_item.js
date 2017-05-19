0cf1b1a9a9fda970df0818dabe827367c7d44dc3
muatik <muatik@gmail.com>
2017-05-19 15:16:14 +0300

    refactors heritage form and implement location selector

--- static/assets/js/create_heritage_item.js
+++ static/assets/js/create_heritage_item.js
@@ -1,96 +1,9 @@
 $(function() {
-
-    var geo_location_pin_map = new Maplace({
-        map_div: '#create_new_heritage_item_pin_gmap',
-        type: 'marker',
-        locations: [{
-            lat: 38.9637,
-            lon: 35.2433,
-            draggable: true,
-            zoom: 5
-        }]
-    }).Load();
-
-    var geo_location_circle_map = new Maplace({
-        map_div: '#create_new_heritage_item_circle_gmap',
-        type: 'circle',
-        locations: [{
-            lat: 38.9637,
-            lon: 35.2433,
-            circle_options: {
-                radius: 150000,
-                editable: true
-            },
-            zoom: 5,
-            draggable: true
-        }]
-    }).Load();
-
-    geo_location_polygon_map = new Maplace({
-        map_div: '#create_new_heritage_item_polygon_gmap',
-        type: 'polygon',
-        locations: [{
-            lat: 39.5,
-            lon: 32.2433,
-            draggable: true,
-            zoom: 5
-        },
-        {
-            lat: 39.5,
-            lon: 38.2433,
-            draggable: true,
-            zoom: 5
-        },
-        {
-            lat: 38,
-            lon: 35.2433,
-            draggable: true,
-            zoom: 5
-        }]
-    }).Load();
-
-    var geo_location_polyline_map = new Maplace({
-        map_div: '#create_new_heritage_item_path_polyline_gmap',
-        type: 'polyline',
-        locations: [{
-            lat: 37,
-            lon: 31,
-            draggable: true,
-            zoom: 5
-        },
-        {
-            lat: 39.5,
-            lon: 33,
-            draggable: true,
-            zoom: 5
-        },
-        {
-            lat: 41,
-            lon: 34,
-            draggable: true,
-            zoom: 5
-        },
-        {
-            lat: 37,
-            lon: 36,
-            draggable: true,
-            zoom: 5
-        },
-        {
-            lat: 40,
-            lon: 38,
-            draggable: true,
-            zoom: 5
-        },
-        {
-            lat: 38.9637,
-            lon: 40,
-            draggable: true,
-            zoom: 5
-        }]
-    }).Load();
-
-
+    var geo_location_polyline_map;
+    var geo_location_polygon_map;
+    var geo_location_circle_map;
+    geo_location_pin_map = 2;
+    map = 1;
 
     AnyTime.picker( "create_new_heritage_item_start_date",
         {
@@ -131,26 +44,116 @@ $(function() {
 
     $( "#create_new_heritage_item_select_map_type" ).change(function() {
         var selected_map_type = $( "#create_new_heritage_item_select_map_type" ).val();
-        if (selected_map_type == "not-selected") {
-            $( ".create_new_heritage_item_map_container" ).css( "display", "none" );
-        }
+        showMapSelector(selected_map_type);
+    });
+
+    function showMapSelector(selected_map_type) {
+        $( ".create_new_heritage_item_map_container").removeClass("active")
+
         if (selected_map_type == "pin") {
-            $( ".create_new_heritage_item_map_container" ).css( "display", "none" );
-            $( "#create_new_heritage_item_pin_gmap_container" ).css( "display", "inline" );
+            $("#create_new_heritage_item_pin_gmap_container").addClass("active");
+            geo_location_pin_map = new Maplace({
+                map_div: '#create_new_heritage_item_pin_gmap',
+                type: 'marker',
+                locations: [{
+                    lat: 38.9637,
+                    lon: 35.2433,
+                    draggable: true,
+                    zoom: 5
+                }]
+            }).Load();
+            map = geo_location_pin_map;
         }
+
         if (selected_map_type == "circle") {
-            $( ".create_new_heritage_item_map_container" ).css( "display", "none" );
-            $( "#create_new_heritage_item_circle_gmap_container" ).css( "display", "" );
+            $("#create_new_heritage_item_circle_gmap_container").addClass("active")
+            geo_location_circle_map = new Maplace({
+                map_div: '#create_new_heritage_item_circle_gmap',
+                type: 'circle',
+                locations: [{
+                    lat: 38.9637,
+                    lon: 35.2433,
+                    circle_options: {
+                        radius: 150000,
+                        editable: true
+                    },
+                    zoom: 5,
+                    draggable: true
+                }]
+            }).Load();
+            map = geo_location_circle_map;
         }
         if (selected_map_type == "polygon") {
-            $( ".create_new_heritage_item_map_container" ).css( "display", "none" );
-            $( "#create_new_heritage_item_polygon_gmap_container" ).css( "display", "" );
+            $("#create_new_heritage_item_polygon_gmap_container").addClass("active")
+            geo_location_polygon_map = new Maplace({
+                map_div: '#create_new_heritage_item_polygon_gmap',
+                type: 'polygon',
+                locations: [{
+                    lat: 39.5,
+                    lon: 32.2433,
+                    draggable: true,
+                    zoom: 5
+                },
+                {
+                    lat: 39.5,
+                    lon: 38.2433,
+                    draggable: true,
+                    zoom: 5
+                },
+                {
+                    lat: 38,
+                    lon: 35.2433,
+                    draggable: true,
+                    zoom: 5
+                }]
+            }).Load();
+            map = geo_location_polygon_map;
         }
         if (selected_map_type == "path-polyline") {
-            $( ".create_new_heritage_item_map_container" ).css( "display", "none" );
-            $( "#create_new_heritage_item_polyline_gmap_container" ).css( "display", "" );
+            $("#create_new_heritage_item_polyline_gmap_container").addClass("active")
+            geo_location_polyline_map = new Maplace({
+                map_div: '#create_new_heritage_item_path_polyline_gmap',
+                type: 'polyline',
+                locations: [{
+                    lat: 37,
+                    lon: 31,
+                    draggable: true,
+                    zoom: 5
+                },
+                {
+                    lat: 39.5,
+                    lon: 33,
+                    draggable: true,
+                    zoom: 5
+                },
+                {
+                    lat: 41,
+                    lon: 34,
+                    draggable: true,
+                    zoom: 5
+                },
+                {
+                    lat: 37,
+                    lon: 36,
+                    draggable: true,
+                    zoom: 5
+                },
+                {
+                    lat: 40,
+                    lon: 38,
+                    draggable: true,
+                    zoom: 5
+                },
+                {
+                    lat: 38.9637,
+                    lon: 40,
+                    draggable: true,
+                    zoom: 5
+                }]
+            }).Load();
+            map = geo_location_polyline_map;
         }
-    });
+    }
 
 
     $('body').on('click', '.add-free-origin-field-but', function() {
@@ -244,23 +247,15 @@ $(function() {
 
         var heritage = {};
 
-        // console.log("Pin type map");
-        // console.dir(geo_location_pin_map);
-        // console.log("Circle type map");
-        // console.dir(geo_location_circle_map);
-        // console.log("Polygon type map");
-        // console.dir(geo_location_polygon_map);
-        // console.log(geo_location_polygon_map.markers[0].position.lat());
-        // console.log(geo_location_polygon_map.markers[0].position.lng());
 
         heritage.title = $( "#create_new_heritage_item_title" ).val();
         heritage.description = $( "#create_new_heritage_item_description" ).val();
         heritage.basicInformation = [];
-        heritage.startDate = "";
-        heritage.endDate = "";
-        heritage.exactDate = "";
+        heritage.origins = [];
+        heritage.startDate = $("#create_new_heritage_item_start_date").val();
+        heritage.endDate = $("#create_new_heritage_item_end_date").val();
+        heritage.exactDate = $("#create_new_heritage_item_exact_date").val();
         heritage.multimedia = [];
-
 
         $(".basicInformation-name-value").each(function(index, nv){
             var name = $(".basicInformation-name", nv).val();
@@ -273,22 +268,45 @@ $(function() {
             }
         });
 
-        var location = {
-            "type": "polygon",
-            "markers": []
-        };
+        $(".create_new_heritage_item_free_origin").each(function(index, origin){
+            var origin = $(origin).val();
+            if (origin) {
+                heritage.origins.push({
+                    "name": origin
+                })
+            }
+        });
 
-        for (var i = geo_location_polygon_map.markers.length - 1; i >= 0; i--) {
-            var marker = geo_location_polygon_map.markers[i];
 
-            location.markers.push({
-                "lat": marker.position.lat(),
-                "long": marker.position.lng()
-            });
+
+
+
+        var selected_map_type = $( "#create_new_heritage_item_select_map_type" ).val();
+
+        // console.log("Pin type map");
+        // console.dir(geo_location_pin_map);
+        // console.log("Circle type map");
+        // console.dir(geo_location_circle_map);
+        // console.log("Polygon type map");
+        // console.dir(geo_location_polygon_map);
+        // console.log(geo_location_polygon_map.markers[0].position.lat());
+        // console.log(geo_location_polygon_map.markers[0].position.lng());
+
+        if (selected_map_type != "not-selected") {
+            var location = {
+                "type": selected_map_type,
+                "markers": []
+            };
+            for (var i = map.markers.length - 1; i >= 0; i--) {
+                var marker = map.markers[i];
+
+                location.markers.push({
+                    "lat": marker.position.lat(),
+                    "long": marker.position.lng()
+                });
+            }
+            heritage.multimedia.push(location);
         }
-
-
-        heritage.multimedia.push(location);
 
         console.log(heritage)
 
@@ -304,7 +322,7 @@ $(function() {
     });
 
 
-
+    showMapSelector("pin")
 
 
 
