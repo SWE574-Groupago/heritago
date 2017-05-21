facfaccf29b995026bafd548d288e3a28f6f3618
fkucuk <fkucuk@gmail.com>
2017-05-21 22:49:00 +0300

    mocking in tests

--- heritago/heritages/tests/tests_annotationdatamodel.py
+++ heritago/heritages/tests/tests_annotationdatamodel.py
@@ -1,10 +1,53 @@
 import unittest
+import requests
 from django.test import Client
+from unittest.mock import Mock, patch
+
+api_url = "/api/v1/annotations/"
+domain = "http://heritago.com/"
+annotation_id = domain + "testanno"
+
+
+def get_annotation():
+    response = requests.get()
+    if response.ok:
+        return response
+    else:
+        return None
 
 
 class AnnotationDataModelTests(unittest.TestCase):
-    def setUp(self):
-        self.client = Client()
+    @classmethod
+    def setup_class(cls):
+        cls.mock_get_patcher = patch('requests.get')
+        cls.mock_get = cls.mock_get_patcher.start()
+
+    @classmethod
+    def teardown_class(cls):
+        cls.mock_get_patcher.stop()
+
+    def test_annotation_must_have_1_or_more_context_property_mocked(self):
+        self.mock_get.return_value.ok = True
+        annotation = [{
+                 '@context': 'http://www.w3.org/ns/anno.jsonld',
+                 'id': annotation_id,
+                 'type': 'Annotation',
+                 'body': 'http://example.org/note1',
+                 'target': {
+                     'source': 'http://example.org/page1.html',
+                     'selector': {
+                         'type': 'XPathSelector',
+                         'value': '/html/body/p[2]/table/tr[2]/td[3]/span'
+                     }
+                 }
+             }]
+        self.mock_get.return_value = Mock()
+        self.mock_get.return_value.json.return_value = annotation
+
+        response = get_annotation()
+        print(response)
+
+
 
     """
     EXAMPLE 1: Basic Annotation Model
@@ -56,4 +99,28 @@ class AnnotationDataModelTests(unittest.TestCase):
       }
     }
     """
+
+
+@unittest.skipIf(False, 'Skipping tests that hit the real API server.')
+def test_integration_contract():
+    # Call the service to hit the actual API.
+    actual = get_annotation()
+    actual_keys = actual.json().pop().keys()
+
+    # Call the service to hit the mocked API.
+    with patch('requests.get') as mock_get:
+        mock_get.return_value.ok = True
+        mock_get.return_value.json.return_value = [{
+            'userId': 1,
+            'id': 1,
+            'title': 'Make the bed',
+            'completed': False
+        }]
+
+        mocked = get_annotation()
+        mocked_keys = mocked.json().pop().keys()
+
+    # An object from the actual API and an object from the mocked API should have
+    # the same data structure.
+
 
