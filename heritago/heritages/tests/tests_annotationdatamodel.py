05f7e68c5d87e5094364cacb5b7d1eecb6f2dc1f
fkucuk <fkucuk@gmail.com>
2017-05-23 19:06:37 +0300

    annotation_id bug resolved.

--- heritago/heritages/tests/tests_annotationdatamodel.py
+++ heritago/heritages/tests/tests_annotationdatamodel.py
@@ -1,26 +1,34 @@
 import unittest
-import requests
 from django.test import Client
 
 api_url = "/api/v1/annotations/"
 domain = "http://heritago.com/"
-
-
-def get_annotation():
-    response = requests.get()
-    if response.ok:
-        return response
-    else:
-        return None
+annotation_id = ""
 
 
 class AnnotationDataModelTests(unittest.TestCase):
     def setUp(self):
         self.Client = Client()
-        annotation_id = domain + "testanno"
+
+    """
+    EXAMPLE 22: XPath Selector
+       {
+          "@context": "http://www.w3.org/ns/anno.jsonld",
+          "id": "http://example.org/anno22",
+          "type": "Annotation",
+          "body": "http://example.org/note1",
+          "target": {
+            "source": "http://example.org/page1.html",
+            "selector": {
+              "type": "XPathSelector",
+              "value": "/html/body/p[2]/table/tr[2]/td[3]/span"
+            }
+          }
+        }
+    """
+    def test_create_XPATH_annotation(self):
         annotation = [{
             '@context': 'http://www.w3.org/ns/anno.jsonld',
-            'id': annotation_id,
             'type': 'Annotation',
             'body': 'http://example.org/note1',
             'target': {
@@ -32,62 +40,36 @@ class AnnotationDataModelTests(unittest.TestCase):
             }
         }]
 
-        response = self.Client.post(api_url, annotation)
-        print(response.json())
-        return response
-
-    """
-    EXAMPLE 1: Basic Annotation Model
-    {
-      "@context": "http://www.w3.org/ns/anno.jsonld",
-      "id": "http://example.org/anno1",
-      "type": "Annotation",
-      "body": "http://example.org/post1",
-      "target": "http://example.com/page1"
-    }
-    """
-    def test_create_annotation(self):
-        annotation_id = domain + "testanno"
-        annotation = [{
-            '@context': 'http://www.w3.org/ns/anno.jsonld',
-            'id': annotation_id,
-            'type': 'Annotation',
-            'body': 'http://example.org/note1',
-            'target': {
-                'source': 'http://example.org/page1.html',
-                'selector': {
-                    'type': 'XPathSelector',
-                    'value': '/html/body/p[2]/table/tr[2]/td[3]/span'
-                }
-            }
-        }]
-
-        response = self.Client.post(api_url, annotation)
-        print(response.json())
+        response = self.Client.post(api_url, annotation).json()
+        return response['id']
 
     def test_annotation_must_have_1_or_more_context_property(self):
         # TODO: code!: https://www.w3.org/TR/annotation-model/ 3.1 Annotations Example-1
         """The Annotation must have 1 or more @context values and http://www.w3.org/ns/anno.jsonld must be one of them. If there is only one value, then it must be provided as a string. PROPERTY: @context)        """
-        response = self.Client.get(api_url + "/" + "testanno")
-        print(response)
+        response = self.Client.get(annotation_id).json()
+        self.assertTrue("@context" in response.keys())
 
     def test_an_annotation_must_have_exactly_1_IRI_that_defines_it(self):
         # TODO: code! https://www.w3.org/TR/annotation-model/ 3.1 Annotations Example-1
         """An Annotation must have exactly 1 IRI that identifies it. PROPERTY: id"""
-        raise NotImplementedError
+        response = self.Client.get(annotation_id).json()
+        self.assertTrue("id" in response.keys())
 
     def test_an_annotation_must_have_1_or_more_types_and_the_annotation_class_must_be_one_of_them(self):
         # TODO: code! https://www.w3.org/TR/annotation-model/ 3.1 Annotations Example-1
         """An Annotation must have 1 or more types, and the Annotation class must be one of them."""
-        raise NotImplementedError
+        response = self.Client.get(annotation_id).json()
+        self.assertTrue(response["type"], "Annotation")
 
     def test_there_should_be_1_or_more_body_relationships_associated_with_an_annotation_but_there_may_be_0(self):
         # TODO: code! https://www.w3.org/TR/annotation-model/ 3.1 Annotations Example-1
-        raise NotImplementedError
+        response = self.Client.get(annotation_id).json()
+        self.assertTrue("body" in response.keys())
     
     def test_there_must_be_1_or_more_target_relationships_associated_with_an_annotation(self):
         # TODO: code! https://www.w3.org/TR/annotation-model/ 3.1 Annotations Example-1
-        raise NotImplementedError
+        response = self.Client.get(annotation_id).json()
+        self.assertTrue("target" in response.keys())
 
     """
     EXAMPLE 22: XPath Selector
